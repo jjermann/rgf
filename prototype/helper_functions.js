0f8c9f2ac1786a7c9fb6c9d195033dfe777786e3
Jonas Jermann <jjermann@gmx.net>
2012-03-09 12:00:17 +0100

    renamed misc.js to helper_functions.js and moved the parts corresponding to the prototype demo into a separate file there...

--- prototype/helper_functions.js
+++ prototype/helper_functions.js
@@ -1,29 +1,3 @@
-function pathToArray(path) {
-    if (path=="" || path==undefined) return [];
-    else if (typeof path=='string') return path.split('.');
-    else return path;
-}
-
-function extend(from,to) {
-    for (var prop in to) {
-        from[prop] = to[prop];
-    };
-    return from;
-};
-
-function deepclone(o) {
-    return (o && typeof(o) === 'object' ?
-    function(t) {
-        for (var p in o) {
-            if (o.hasOwnProperty(p)) {
-                t[p] = deepclone(o[p])
-            }
-        }
-        return t
-    }({}) : o)
-}
-
-// only for testing
 function createBox(id,title,style) {
     var el,tmp;
     var titleHeight=20;
